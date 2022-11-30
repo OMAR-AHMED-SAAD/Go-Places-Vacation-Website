c72736e0696a4d5131a96ebee4ce8cace981a2c9
Omar Ahmed <omarahmedaww@gmail.com>
2022-12-01 01:39:31 +0200

    third commit

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const { MongoClient } = require('mongodb');
 const client = new MongoClient("mongodb://localhost:27017"); // Define my MongoDB cluster "NEEDS UPDATE WHEN HOSTED"
 client.connect(); // Connect to the MongoDB cluster
 const db = client.db('MyDB');
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -41,12 +40,11 @@ app.get('/registration', function (req, res) {
 app.post('/register', function (req, res) {
     var user = req.body.username;
     var pass = req.body.password;
+    if (pass == "" || user == "")
+        return res.send({ 'error': "CAN NOT HAVE EMPTY USERNAME OR PASSWORD" });
     db.collection('User&Pass').findOne({ username: user }, function (er, result) {
         if (result != null) {
             return res.send({ 'error': "SOMEONE ELSE HAS the same USERNAME" });
-        }
-        else if (pass == null) {
-            return res.send({ 'error': "CAN NOT HAVE EMPTY PASSWORD" });
         }
         else {
             db.collection('User&Pass').insertOne({ username: user, password: pass });
@@ -87,40 +85,32 @@ app.get('/bali', function (req, res) {
 app.get('/santorini', function (req, res) {
     res.render('santorini');
 });
-app.get('/search', function (req, res) {
-    res.render('searchresults');
-});
 app.get('/wanttogo', function (req, res) {
     res.render('wanttogo');
 });
 
+var searched_destiantions = [];
+app.get('/search', function (req, res) {
+    res.render('searchresults', { data: searched_destiantions });
+});
 
 app.post('/search', function (req, res) {
     var search_value = req.body.Search;
-    var search_value_regular= new RegExp(search_value,"i")
-    db.collection('Destinations').find({ destination:search_value_regular}).toArray(function (er, result) {
+    var search_value_regular = new RegExp(search_value, "i");
+    db.collection('Destinations').find({ destination: search_value_regular }).toArray(function (er, result) {
         if (result.length == 0) {
-           return res.send({ 'error': "Destination not Found" })
+            return res.send({ 'error': "Destination not Found" })
         }
-        else{
-            res.redirect('/search');
-            console.log(result);
+        else {
+            //message of sucess
+            //manage empty search case with a message too
+            searched_destiantions = result;
+            return res.redirect('/search');
         }
-        
+
     });
 });
-// function test(){
-//     var x=new RegExp("in", "i");
-// db.collection('Destinations').find({ destination:x }).toArray(function (er, result) {
-//     if (result.length == 0) {
-//         res.send({ 'error': "Destination not Found" })
-//     }
-//     console.log();
-//         console.log(result);
-    
-// });
-// }
-//  test();
+
 app.listen(8080);
 
 
@@ -130,3 +120,5 @@ app.listen(8080);
 
 
 // need to manage all message of failure and success with their redirections
+//need to manage empty search 
+//empty login creditionals remove them from db
